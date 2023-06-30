ad45ed04505221e1f78687ce9c1e8e3e25f0a150
Nastassia Ilyich <nilyich@axionbio.com>
2023-06-30 14:47:20 +0400

    Add project files.

---
+++ Froggy/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Froggy
+{
+    public partial class MainWindow : Window
+    {
+        private bool isDragging;
+        private Point startPoint;
+
+        private DispatcherTimer timer;
+        private DateTime startTime;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += Timer_Tick; // Wire up the event handler
+        }
+
+        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (e.ClickCount == 2 && e.OriginalSource is Image)
+                {
+                    StartTimer();
+                }
+                else
+                {
+                    isDragging = true;
+                    startPoint = e.GetPosition(this);
+                    Mouse.Capture(this);
+                }
+            }
+        }
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point currentPosition = e.GetPosition(this);
+                double offsetX = currentPosition.X - startPoint.X;
+                double offsetY = currentPosition.Y - startPoint.Y;
+
+                // Update the position of the window
+                Left += offsetX;
+                Top += offsetY;
+            }
+        }
+
+        private void MainWindow_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (isDragging)
+            {
+                isDragging = false;
+                Mouse.Capture(null);
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = DateTime.Now - startTime;
+            timerLabel.Content = elapsed.ToString(@"mm\:ss");
+        }
+
+        private void StartTimer()
+        {
+            startTime = DateTime.Now;
+            timer.Start(); // Start the timer
+        }
+
+        private void StopTimer()
+        {
+            timer.Stop();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            StartTimer();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            StopTimer();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,54 @@
+# Froggy Application
+
+This application is a simple frog-themed desktop application built using the WPF framework in C#. It allows users to interact with a window and perform actions such as dragging the window and starting/stopping a timer.
+
+## Features
+
+The Froggy application provides the following features:
+
+1. Dragging the Window: Users can click and drag the application window to move it around the screen.
+
+2. Timer Functionality: Users can double-click on the frog image within the window to start a timer. The timer updates every 100 milliseconds and displays the elapsed time in minutes and seconds.
+
+## Prerequisites
+
+To run the Froggy application, ensure that you have the following:
+
+- Windows operating system.
+- .NET Framework installed.
+
+## Getting Started
+
+To get started with the Froggy application, follow these steps:
+
+1. Clone the repository or download the source code files.
+
+2. Open the project in Visual Studio or any other compatible C# development environment.
+
+3. Build the project to restore dependencies and compile the application.
+
+4. Run the application, and the Froggy window will appear.
+
+## How to Use
+
+Once the Froggy application is running, you can perform the following actions:
+
+- Dragging the Window:
+  - Click and hold the left mouse button anywhere inside the window.
+  - Move the mouse to drag the window around the screen.
+  - Release the mouse button to drop the window in the desired position.
+
+- Starting the Timer:
+  - Double-click on the frog image inside the window.
+  - The timer will start, and the elapsed time will be displayed in the label.
+
+- Stopping the Timer:
+  - To stop the timer, simply close the Froggy window.
+
+## Contributions
+
+Contributions to the Froggy application are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or submit a pull request.
+
+## Contact
+
+Thank you for using the Froggy application!
